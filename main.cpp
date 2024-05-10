c56a6c797cee3828dbd93fd8e3a8324c9e2c59e0
AaronSledge <ahern2118@gmail.com>
2024-05-10 10:08:17 -0700

    Getting started on main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main() {
+    //Make library object
+
+    char start;
+
+    std::cout << "Press s to sign in" << std::endl;
+
+    do {
+        std::cin >> start;
+
+    } while(start != 's');
+
+    //do login system
+    
+}
