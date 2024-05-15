2251af41dae4cb1ac79994f3e7ca3c096f47e432
AaronSledge <ahern2118@gmail.com>
2024-05-15 08:37:01 -0700

    Create registration branch

--- main.cpp
+++ main.cpp
@@ -1,19 +1,52 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
 
 
+
+void registerFirstTime(bool isUser) {
+    
+}
 
 int main() {
     //Make library object
 
     char start;
+    bool isUser = false;
+    string userOrAdmin;
+    string signinFirstTime;
 
-    std::cout << "Press s to sign in" << std::endl;
+    cout << "Press s to sign in" << endl;
 
     do {
         std::cin >> start;
 
     } while(start != 's');
 
-    //do login system
+    cout << "Are you a user or admin" << endl;
     
+    if(not(cin >> userOrAdmin)) {
+        throw std::runtime_error("could not read input");
+    }
+    
+    if(userOrAdmin == "user") {
+        isUser = true;
+    }
+
+    cout << "Are you signing in for the first time" << endl;
+    if(not(cin >> signinFirstTime)) {
+        throw std::runtime_error("could not read input");
+    }
+
+    if(signinFirstTime == "yes") {
+        registerFirstTime(isUser);
+    }
+    else {
+        //sign in function
+    }
 }
