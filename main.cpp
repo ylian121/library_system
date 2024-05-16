315c679635e94e9ba8bd0a0f874eeac4ddc70ccd
AaronSledge <ahern2118@gmail.com>
2024-05-15 20:41:42 -0700

    basic function of registration

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <stdexcept>
@@ -8,8 +9,42 @@ using std::cout;
 using std::endl;
 
 
+void checkSameUserName(string &inputUserName) {
+    string userName;
+    std::ifstream inFS;
+    inFS.open("username.txt");
 
-void registerFirstTime(bool isUser) {
+    //check if file can be open
+    if(!(inFS.is_open())) {
+        throw std::runtime_error("couldn't open username.txt");
+
+    }
+
+    //read through file untill a match is found
+    while(inFS >> userName) {
+        //if there is a match re enter userName and call function again
+        if(userName == inputUserName) {
+            cout << "this user name has been taken. Please enter another." << endl;
+            cin >> inputUserName;
+            checkSameUserName(inputUserName);
+        }
+    }
+
+    inFS.close();
+
+    std::ofstream outFS;
+    outFS.open("username.txt", std::ios::app);
+
+    //check if userName file can be open
+    if(!(outFS.is_open())) {
+        throw std::runtime_error("couldn't open username.txt");
+    }
+
+    outFS << inputUserName << endl;
+    outFS.close();
+}
+
+void registerFirstTime(bool isUser, const string &fullName, string &passWord, string &inputUserName) {
     //first ask for name, password, and username
     //create txt file usernames, store username
     //when we get input, we read username.txt file and see if there is any matches
@@ -17,8 +52,49 @@ void registerFirstTime(bool isUser) {
     //once we see there is no matches, create a new txt file called <username>.txt
     //each person's txt file is going to contain 3 things, name, password, and username
     //send those 3 things into that particular file
-    
+
+    //variable declaration
+    string userFilePath = "users/";
+    string adminFilePath = "admins/";
+
+
+    //check if pass word is the same before creating txt files
+    checkSameUserName(inputUserName);
+
+    //add in password checker
+
+
+    //if the person is a user, go through the user filePath
+    if(isUser == true) {
+        userFilePath = userFilePath + inputUserName + ".txt";
+        std::ofstream userOut(userFilePath, std::ios::app);
+        
+        //check if file can be open
+        if(!(userOut.is_open())) {
+            throw std::runtime_error("couldn't open <userName>.txt");
+        }
+
+
+        userOut << inputUserName << endl;
+        userOut << passWord << endl;
+        userOut << fullName << endl;
+        userOut.close();
+    }
+    //if the person is an admin, go through admin filePath
+    else {
+        adminFilePath = adminFilePath + inputUserName + ".txt";
+        std::ofstream adminOut(adminFilePath, std::ios::app);
+        if(!(adminOut.is_open())) {
+            throw std::runtime_error("couldn't open <userName>.txt");
+        }
+
+        adminOut << inputUserName << endl;
+        adminOut << passWord << endl;
+        adminOut << fullName << endl;
+        adminOut.close();
+    }
 }
+
 
 /*void signin(bool isUser) {
     //ask username, password
@@ -36,6 +112,9 @@ int main() {
     bool isUser = false;
     string userOrAdmin;
     string signinFirstTime;
+    string fullName;
+    string inputUserName;
+    string passWord;
 
     cout << "Press s to sign in" << endl;
 
@@ -59,8 +138,17 @@ int main() {
         throw std::runtime_error("could not read input");
     }
 
+    cout << "please enter your name" << endl;
+    cin >> fullName;
+    
+    cout << "please enter your password" << endl;
+    cin >> passWord;
+
+    cout << "please enter a userName" << endl;
+    cin >> inputUserName;
+
     if(signinFirstTime == "yes") {
-        registerFirstTime(isUser);
+        registerFirstTime(isUser, fullName, passWord, inputUserName);
         //create user or admin object
     }
     else {
