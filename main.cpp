bbe0e8248ce84bd73e0f2a4bf63aef3027ed34a9
AaronSledge <ahern2118@gmail.com>
2024-05-15 17:43:28 -0700

    Added notes on registration/signin

--- main.cpp
+++ main.cpp
@@ -10,9 +10,25 @@ using std::endl;
 
 
 void registerFirstTime(bool isUser) {
+    //first ask for name, password, and username
+    //create txt file usernames, store username
+    //when we get input, we read username.txt file and see if there is any matches
+    //make a folder call user and admin
+    //once we see there is no matches, create a new txt file called <username>.txt
+    //each person's txt file is going to contain 3 things, name, password, and username
+    //send those 3 things into that particular file
     
 }
 
+/*void signin(bool isUser) {
+    //ask username, password
+    //first check if username is actually there in the txt.file
+    //if not input them to type again
+    //then check <username>.txt file and see if password == input(password is 2nd line)
+    //if not equal try again
+
+}
+*/
 int main() {
     //Make library object
 
@@ -45,8 +61,16 @@ int main() {
 
     if(signinFirstTime == "yes") {
         registerFirstTime(isUser);
+        //create user or admin object
     }
     else {
         //sign in function
+        //create user or admin object
+        //login()
     }
+
+    /*if(isUser == true) {
+        //create user object
+        //print usermenu...
+    }*/
 }
