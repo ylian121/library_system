be9d0f71b140f55868528dcec28df52ef2683810
haasha600 <haardbshah@gmail.com>
2024-06-03 13:42:47 -0700

    Book class added

--- header/Book.hpp
+++ header/Book.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class Book{
 private:
     string name;
+    bool checkedOut;
+    vector<string> genres;
 public:
+    Book(const string& name): name(name), checkedOut(false){}
     const string& getName() const
     {
         return this->name;
     }
+    bool containsGenre(const string& genre) const;
+    void addGenre(const string& genre);
+    void removeGenre(const string& genre);
+    void checkOut();
+    void checkIn();
 };
---
+++ src/Book.cpp
@@ -0,0 +1,54 @@
+#include "Book.hpp"
+
+#include <stdexcept>
+
+using std::runtime_error;
+
+bool Book::containsGenre(const string& genre) const{
+    for(string currGenre:genres){
+        if(genre == currGenre){
+            return true;
+        }
+    }
+    return false;
+}
+void Book::addGenre(const string& genre){
+    if(!(this->containsGenre(genre)))
+    {
+        this->genres.push_back(genre);
+    }
+}
+void Book::removeGenre(const string& genre){
+    if(this->containsGenre(genre)){
+        string currGenre;
+        bool contains= true;
+        for(unsigned i = 0; i+1<genres.size() && contains==true; ++i){
+            currGenre = genres.at(i);
+            if (currGenre==genre)
+            {
+                for (unsigned j = i; j+1<genres.size(); j++)
+                {
+                    genres.at(j) = genres.at(j+1);
+                }
+                contains = false;
+            }
+        }
+        genres.pop_back();
+    }
+}
+void Book::checkOut(){
+    if(!checkedOut){
+        checkedOut=true;
+    }
+    else{
+        throw runtime_error("trying to checkout a book that has already been checked out");
+    }
+}
+void Book::checkIn(){
+    if(checkedOut){
+        checkedOut=false;
+    }
+    else{
+        throw runtime_error("trying to check in a book that has not yet been checked out");
+    }
+}
